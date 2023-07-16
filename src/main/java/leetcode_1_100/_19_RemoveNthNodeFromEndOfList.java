aa9521e3722cbeb7aa022bd39fd4b6385d9be415
Vijay Inani <vijayinani@users.noreply.github.com>
2023-07-16 18:17:05 +0530

    19

---
+++ src/main/java/leetcode_1_100/_19_RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,59 @@
+package leetcode_1_100;
+
+public class _19_RemoveNthNodeFromEndOfList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        int i = 0;
+        while (i <= n) {
+            p1 = p1.next;
+            i++;
+        }
+        //if (p1 == null) {
+        //    return head.next;
+        //}
+        //p1 = p1.next;
+        while (p1 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        p2.next = p2.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] values = new int[]{1, 2, 3, 4, 5};
+        ListNode head = new ListNode(-1);
+        ListNode dummy = head;
+        for (int val : values) {
+            ListNode curr = new ListNode(val);
+            head.next = curr;
+            head = curr;
+        }
+
+        new _19_RemoveNthNodeFromEndOfList().removeNthFromEnd(dummy.next, 2);
+
+        while (dummy != null) {
+            System.out.println(dummy.val);
+            dummy = dummy.next;
+        }
+    }
+}

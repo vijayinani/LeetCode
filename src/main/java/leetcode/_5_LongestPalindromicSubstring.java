ee8901d44732a8e90664b66d61dd0e712e7afd7d
Vijay Inani <vijayinani@users.noreply.github.com>
2023-07-16 09:55:26 +0530

    5th solved

--- src/main/java/leetcode/_5_LongestPalindromicSubstring.java
+++ src/main/java/leetcode/_5_LongestPalindromicSubstring.java
@@ -1,7 +1,7 @@
 package leetcode;
 
 public class _5_LongestPalindromicSubstring {
-    public String longestPalindrome(String s) {
+    /*public String longestPalindrome(String s) {
         int n = s.length();
         int startIndex = 0;
         int maxLength = 0;
@@ -16,6 +16,29 @@ public class _5_LongestPalindromicSubstring {
             }
         }
         return s.substring(startIndex, startIndex + maxLength);
+    }*/
+
+    int maxLength = 0;
+    int startIndex = 0;
+
+    public String longestPalindrome(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            check(s, i, i);
+            check(s, i, i + 1);
+        }
+        return s.substring(startIndex, startIndex + maxLength);
+    }
+
+    private void check(String s, int begin, int end) {
+        while (begin >= 0 && end < s.length() && s.charAt(begin) == s.charAt(end)) {
+            begin--;
+            end++;
+        }
+
+        if (maxLength < end - begin - 1) {
+            startIndex = begin + 1;
+            maxLength = end - begin - 1;
+        }
     }
 
     public static void main(String[] args) {

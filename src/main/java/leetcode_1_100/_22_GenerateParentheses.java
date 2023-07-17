7de4c6c6e8336546b99a383a296ee94fb5a46223
Vijay Inani <vijayinani@users.noreply.github.com>
2023-07-17 07:28:07 +0530

    done

---
+++ src/main/java/leetcode_1_100/_22_GenerateParentheses.java
@@ -0,0 +1,30 @@
+package leetcode_1_100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _22_GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, 0, 0, "", n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, int open, int close, String s, int max) {
+
+        if (s.length() == max * 2) {
+            result.add(s);
+        }
+
+        if (open < max) {
+            backtrack(result, open + 1, close, s + "(", max);
+        }
+        if (close < open) {
+            backtrack(result, open, close + 1, s + ")", max);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new _22_GenerateParentheses().generateParenthesis(3));
+    }
+}
